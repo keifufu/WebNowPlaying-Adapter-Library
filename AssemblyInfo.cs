4ed5c5c7bcc7d16e8262ff795928c42f9c9b782f
keifufu <keifufu@noonly.net>
2023-02-28 07:24:11 +0100

    nuget pkg

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System.Reflection;
 
+[assembly: AssemblyTitle("WNPRedux-Adapter-Library")]
+[assembly: AssemblyDescription("A C# library to communicate with the WebNowPlaying-Redux extension")]
+[assembly: AssemblyCompany("keifufu")]
+[assembly: AssemblyProduct("WNPRedux-Adapter-Library")]
 [assembly: AssemblyCopyright("Copyright © keifufu 2023")]
 [assembly: AssemblyVersion("1.0.0")]
